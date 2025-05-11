f852bd14c72d09689003a5284f76c30d14a3446c
Adam Kraś <adam.kras.stud@pw.edu.pl>
2025-05-11 23:28:37 +0200

    Finalized main and NPCs

---
+++ haskell/Main.hs
@@ -0,0 +1,133 @@
+-- Main.hs
+{-# LANGUAGE FlexibleContexts #-}
+import DataTypes
+import GameData
+import Actions
+import NPCs -- For handleTalk
+import Utils
+
+import Control.Monad.State
+import System.IO (hFlush, stdout)
+import System.Random (getStdRandom, randomR, newStdGen, StdGen)
+import Data.Char (toLower, isDigit)
+import Data.List (isPrefixOf)
+import qualified Data.Map as M
+import System.Exit (exitSuccess)
+
+
+-- Initialize game with random strength and money
+initializeGame :: IO GameState
+initializeGame = do
+    initialStr <- getStdRandom (randomR (50, 100)) -- Prolog: random(50, 101, S) -> S in [50, 100]
+    initialMon <- getStdRandom (randomR (0, 150))  -- Prolog: random(0, 151, M) -> M in [0, 150]
+    let player = initialPlayer { strength = initialStr, money = initialMon }
+    return $ initialGameState player
+
+-- Parse player input into a Command
+parseCommand :: String -> Command
+parseCommand input =
+    let tokens = words $ map toLower input
+    in case tokens of
+        ["go", loc] -> Go loc
+        ["go", loc1, loc2] -> Go (loc1 ++ " " ++ loc2) -- For two-word locations like "strefa cardio"
+        ["take", item] -> Take item
+        ["take", item1, item2] -> Take (item1 ++ " " ++ item2)
+        ["drop", item] -> Drop item
+        ["drop", item1, item2] -> Drop (item1 ++ " " ++ item2)
+        ["inventory"] -> Inventory
+        ["inv"] -> Inventory
+        ["consume", item] -> Consume item
+        ["consume", item1, item2] -> Consume (item1 ++ " " ++ item2)
+        ["talk", npc] -> Talk npc
+        ["talk", npc1, npc2] -> Talk (npc1 ++ " " ++ npc2)
+        ["buy", item] -> Buy item
+        ["buy", item1, item2] -> Buy (item1 ++ " " ++ item2)
+        ["wear", item] -> Wear item
+        ["wear", item1, item2] -> Wear (item1 ++ " " ++ item2)
+        ["look"] -> Look
+        ["instructions"] -> Instructions
+        ["help"] -> Instructions
+        ["money"] -> CheckMoney
+        ["check_money"] -> CheckMoney
+        ["weights"] -> ShowWeightInventory
+        ["weight_inventory"] -> ShowWeightInventory
+
+        -- Bench commands
+        ["take_bench"] -> TakeBench
+        ["left_add", wStr] | all isDigit wStr ->
+            case stringToWeight wStr of Just w -> LeftAddWeight w; _ -> Unknown input
+        ["right_add", wStr] | all isDigit wStr ->
+            case stringToWeight wStr of Just w -> RightAddWeight w; _ -> Unknown input
+        ["remove_all_weights"] -> RemoveAllWeightsBench -- Shortened from Prolog for ease
+        ["remove_weights"] -> RemoveAllWeightsBench
+        ["check_bench"] -> CheckBench
+        ["do_bench_press"] -> DoBenchPress
+        ["komplementuj"] -> KomplementujRecepcjonistke -- shorthand
+        ["komplementuj", "recepcjonistke"] -> KomplementujRecepcjonistke
+
+
+        ["quit"] -> Quit
+        ["q"] -> Quit
+        _ -> Unknown input
+
+
+-- Process a command and update game state
+processCommand :: Command -> StateT GameState IO ()
+processCommand cmd = do
+    messages <- case cmd of
+        Go locName               -> handleGo locName
+        Take itemName            -> handleTake itemName
+        Drop itemName            -> handleDrop itemName
+        Inventory                -> handleInventory
+        Consume itemName         -> handleConsume itemName
+        Talk npcName             -> handleTalk npcName
+        Buy itemName             -> handleBuy itemName
+        Wear itemName            -> handleWear itemName
+        Look                     -> handleLook
+        Instructions             -> handleInstructions
+        CheckMoney               -> handleCheckMoney
+        ShowWeightInventory      -> handleShowWeightInventory
+
+        TakeBench                -> handleTakeBench
+        LeftAddWeight w          -> handleLeftAddWeightBench w
+        RightAddWeight w         -> handleRightAddWeightBench w
+        RemoveAllWeightsBench    -> handleRemoveAllWeightsBench
+        CheckBench               -> handleCheckBench
+        DoBenchPress             -> handleDoBenchPress
+        KomplementujRecepcjonistke -> handleKomplementujRecepcjonistke
+
+        Quit                     -> liftIO (putStrLn "Do zobaczenia!") >> liftIO exitSuccess >> return []
+        Unknown s                -> return ["Nie rozumiem '" ++ s ++ "'. Wpisz 'help' po listę komend."]
+
+    liftIO $ mapM_ putStrLn messages
+
+-- Main game loop
+gameLoop :: StateT GameState IO ()
+gameLoop = do
+    gs <- get
+    -- Check for game over conditions not tied to specific actions (e.g. if score or stage triggers it)
+    -- (Currently, game over is handled within actions like DoBenchPress, Consume, Go SzatniaDamska)
+
+    liftIO $ putStr "> "
+    liftIO $ hFlush stdout
+    line <- liftIO getLine
+    let cmd = parseCommand line
+    processCommand cmd
+    gameLoop -- Loop indefinitely until Quit or exitSuccess
+
+-- Start the game
+main :: IO ()
+main = do
+    putStrLn "Witaj w 'Klata, biceps, barki'!"
+    putStrLn "Twoim celem jest odbycie treningu na siłowni."
+    putStrLn "Zbierz potrzebny ekwipunek z domu, wejdź na siłownię i wykonaj ćwiczenia!"
+    putStrLn "Wpisz 'help' aby zobaczyć listę komend."
+    putStrLn "---------------------------------------------------------------------"
+
+    initialGs <- initializeGame
+
+    -- Run the initial 'look' for the player
+    (initialMessages, s0) <- runStateT handleLook initialGs
+    mapM_ putStrLn initialMessages
+
+    evalStateT gameLoop s0
---
+++ haskell/NPCs.hs
@@ -0,0 +1,145 @@
+-- NPCs.hs
+module NPCs where
+
+import DataTypes
+import GameData
+import Actions -- For startStage, addWeightsToPlayer, handleFinish
+import Utils
+import Control.Monad.State
+import qualified Data.Map as M
+import qualified Data.Set as S
+import Data.Maybe (fromMaybe)
+
+
+handleTalk :: String -> GameAction
+handleTalk npcNameStr = do
+    gs <- get
+    let currentLoc = currentLocation gs
+    let pState = playerState gs
+
+    case stringToNPC npcNameStr of
+        Nothing -> return ["Nie ma tu kogoś takiego jak '" ++ npcNameStr ++ "'."]
+        Just npc ->
+            if not (S.member npc (activeNPCs gs) && M.lookup npc (npcLocations gs) == Just currentLoc)
+            then return ["Nie widzisz tutaj osoby: " ++ npcDisplayName npc ++ "."]
+            else -- NPC is here and active, proceed with interaction
+                case npc of
+                    Recepcjonistka -> interactRecepcjonistka
+                    PodejrzanyTyp  -> interactPodejrzanyTyp
+                    ChudySzczur    -> interactChudySzczur
+                    Brunetka       -> interactBrunetka
+                    CzlowiekSzczuply -> interactCzlowiekSzczuply
+                    SzczurBojowy   -> interactSzczurBojowy
+                    DuzyChlop      -> interactDuzyChlop
+                    WielkiChlop    -> interactWielkiChlop
+                    Swiezak        -> interactSwiezak
+
+interactRecepcjonistka :: GameAction
+interactRecepcjonistka = return
+    [ "Recepcjonistka:"
+    , "Witaj na siłowni! Czy czegoś ci potrzeba?"
+    , "Możesz tutaj kupić: "
+    , " - monster (buy monster) za 10zł"
+    , " - przedtreningowka (buy przedtreningowka) za 20zł"
+    , " - karnet (buy karnet) za 40zł."
+    ]
+
+interactPodejrzanyTyp :: GameAction
+interactPodejrzanyTyp = return
+    [ "Podejrzany typ:"
+    , "Witaj, czyżbyś szedł na trening? Może chcesz coś mocniejszego, co na pewno poprawi twoje wyniki? Moje produkty są całkowicie bezpieczne!"
+    , "Możesz tutaj kupić: "
+    , " - mala strzykawka (buy mala_strzykawka) za 30zł"
+    , " - duza strzykawka (buy duza_strzykawka) za 50zł."
+    ]
+
+interactChudySzczur :: GameAction
+interactChudySzczur = do
+    -- Assuming Chudy Szczur can only be talked to once for weights.
+    -- We can add a flag to GameState like `chudySzczurTalked :: Bool`
+    -- For now, let's assume he always gives weights if he's active.
+    -- In Prolog, he's always there. We might want to make him disappear or change dialogue after.
+    msgs <- addWeightsToPlayer [5, 5]
+    return $
+        [ "Ty: Ej, mały, potrzebuję tych talerzy 5kg, mogę je zabrać?"
+        , "Chudy szczur: (piszczy nerwowo) Tylko... nie bij mnie! Bierz, co chcesz, i znikaj!"
+        , "Ty: Spoko, luz. Tylko mi je podaj, i będę się zwijał."
+        , "Chudy szczur: (podaje talerze) Masz, i już mnie nie zaczepiaj!"
+        ] ++ msgs ++ ["Dodałeś do swojego ekwipunku ciężary: 5kg i 5kg."]
+
+
+interactBrunetka :: GameAction
+interactBrunetka = return
+    [ "Ty: Cześć, przepraszam cię najmocniej, że przeszkadzam, ale czy będziesz jeszcze używać tych 10kg?"
+    , "Brunetka: Zostaw mnie, mam chłopaka!"
+    , "Ty: Źle zrozumiałaś, ja tylko chciałem..."
+    , "Brunetka: Mam wezwać ochronę? Spadaj!"
+    -- Note: Prolog has `at(talerze_10_kg, strefa_cardio)`. Interaction doesn't give them.
+    -- This implies player might need another way or this NPC is a red herring for 10kg plates.
+    -- The game seems to get weights from other NPCs who *give* them.
+    ]
+
+interactCzlowiekSzczuply :: GameAction
+interactCzlowiekSzczuply = do
+    msgs <- addWeightsToPlayer [15, 15]
+    return $
+        [ "Ty: Stary, te talerze 15kg... mogę je pożyczyć? Na chwilę?"
+        , "Człowiek szczupły: (wzrusza ramionami) No dobra, ale szybko oddaj. Ja tu jeszcze muszę poćwiczyć."
+        , "Ty: Jasne, jasne, tylko je wezmę. Dzięki!"
+        , "Człowiek szczupły: Tylko ich nie zgub, bo będziesz miał ze mną do czynienia!"
+        ] ++ msgs ++ ["Dodałeś do swojego ekwipunku ciężary: 15kg i 15kg."]
+
+interactSzczurBojowy :: GameAction
+interactSzczurBojowy = do
+    msgs <- addWeightsToPlayer [20, 20]
+    return $
+        [ "Ty: Ej, byczku, te 20 kilo... mogę je zabrać?"
+        , "Szczur bojowy: Jasne, stary, bierz. I tak mi się już nie przydadzą."
+        , "Ty: Dzięki, jesteś wielki!"
+        , "Szczur bojowy: Tylko uważaj, żebyś sobie krzywdy nie zrobił. To ciężkie żelastwo."
+        ] ++ msgs ++ ["Dodałeś do swojego ekwipunku ciężary: 20kg i 20kg."]
+
+
+interactDuzyChlop :: GameAction
+interactDuzyChlop = do
+    msgs <- addWeightsToPlayer [25, 25]
+    return $
+        [ "Ty: Mogę zabrać talerze 25 kg?"
+        , "Duży chłop: Spoko, nie ma sprawy, już ich nie używam!"
+        ] ++ msgs ++ ["Dodałeś do swojego ekwipunku ciężary: 25kg i 25kg."]
+
+interactWielkiChlop :: GameAction
+interactWielkiChlop = return
+    [ "Ty: Mogę zabrać talerze..."
+    , "Wielki chłop: Spadaj szczurze!"
+    ]
+
+interactSwiezak :: GameAction
+interactSwiezak = do
+    gs <- get
+    let pState = playerState gs
+    let currentStage = gameStage gs
+
+    if currentStage == 2 then -- Swiezak is asking for bidon
+        if S.member CzerwonyBidon (inventory pState) then do
+            -- Player has the bidon and gives it
+            let newPState = pState { inventory = S.delete CzerwonyBidon (inventory pState) }
+            -- Don't add Expired Przedtreningowka based on stage 3 text
+            put gs { playerState = newPState, gameStage = 3 }
+
+            stage3Msgs <- startStage 3 -- This will generate the "dostałeś przeterminowaną..."
+            return $
+                [ "Ty: Cześć, stary, znalazłem twój czerwony bidon w łazience."
+                , "Swiezak: Dzięki, stary! Nie wiem co bym bez ciebie zrobił!"
+                , "Ty: Nie ma sprawy!"
+                ] ++ stage3Msgs
+        else
+            return
+                [ "Swiezak: I jak znalazłeś mój bidon?"
+                , "Ty: Niestety, jeszcze go nie znalazłem."
+                , "Swiezak: Powodzenia!"
+                ]
+    else if currentStage >= 3 then -- After bidon quest
+        return ["Swiezak: Dzięki jeszcze raz za pomoc z bidonem! Jak trening idzie?"]
+    else -- Should not happen if Swiezak is only active from stage 2
+        return ["Swiezak patrzy na ciebie pytająco."]
